a12e374fb215e3faa65b4f084d20ff49b87dd00b
Daniel Kroon <drg.kroon@gmail.com>
2020-05-19 21:26:28 +0200

    Updates

---
+++ precache-manifest.7c06f3349c209747bd4e12f429154f27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "537c09a1e6c8eabc3870023ff5ca1229",
+    "url": "/io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "72f3b6d42c06da7108e3",
+    "url": "/io/monsters-rolodex/static/css/main.9494d0e7.chunk.css"
+  },
+  {
+    "revision": "0105f5fa7a131123a102",
+    "url": "/io/monsters-rolodex/static/js/2.8a77c072.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/io/monsters-rolodex/static/js/2.8a77c072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72f3b6d42c06da7108e3",
+    "url": "/io/monsters-rolodex/static/js/main.3897f6af.chunk.js"
+  },
+  {
+    "revision": "ba9cde10d6dc0ae9d308",
+    "url": "/io/monsters-rolodex/static/js/runtime-main.f5e9d59c.js"
+  }
+]);
